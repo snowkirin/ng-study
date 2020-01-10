8a23ebaf4cc83dbca576431b1045e7fc948c5ff1
JS <js@adwitt.com>
2020-01-10 17:19:09 +0900

    Event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'This is Title';
+
+  click = () => {
+    this.title = '자식으로 부터 실행됨';
+  };
+  myEvent() {
+    this.title = '자식이 발생한 이벤트에 의해 실행됨';
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { Sample4Component } from './components/sample4/sample4.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, Sample4Component],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
