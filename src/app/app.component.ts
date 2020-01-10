3ea7ec6adc29980c816e915f10226424119decce
JS <js@adwitt.com>
2020-01-10 16:08:45 +0900

    Hello World

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = '앵귤러 스터디';
-}
+export class AppComponent {}
