bbe7fef9b9d43d49c0754d1fcb794a2c94567ea1
JS <js@adwitt.com>
2020-01-13 11:29:11 +0900

    쓸대없는거 체인지

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'This is Title';
-
-  click = () => {
-    this.title = '자식으로 부터 실행됨';
-  };
-  myEvent() {
-    this.title = '자식이 발생한 이벤트에 의해 실행됨';
-  }
 }
