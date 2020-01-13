294e56e239d2fbd7fe65be74b293b66e857e23d7
JS <js@adwitt.com>
2020-01-13 11:25:05 +0900

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { Sample4Component } from './components/sample4/sample4.component';
 
 @NgModule({
-  declarations: [AppComponent, Sample4Component],
+  declarations: [AppComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
